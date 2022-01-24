555ceca66858af549a25e2dfc44d37ca466e19ea
Paweł <pawelpeksa@gmail.com>
2022-01-24 20:37:30 +0100

    lowercase className

--- src/components/HomeThreeColumns.tsx
+++ src/components/HomeThreeColumns.tsx
@@ -3,7 +3,7 @@ import { Column } from "./Column";
 export const HomeThreeColumns = () => {
 
   return (
-  <div className="ThreeColumns__wrapper"> 
+  <div className="threeColumns__wrapper"> 
     <Column number={10} title="ODDANYCH WORKÓW" description="Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat." />
     <Column number={5} title="WSPARTYCH ORGANIZACJI" description="Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat." />
     <Column number={7} title="ZORGANIZOWANYCH ZBIÓREK" description="Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat." />
