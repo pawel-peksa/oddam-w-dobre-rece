d6c5142277222e0e1002d284c4518231a2911163
Paweł <pawelpeksa@gmail.com>
2022-01-25 11:13:27 +0100

    improve scroll to position

--- src/components/HomeFourSimpleSteps.tsx
+++ src/components/HomeFourSimpleSteps.tsx
@@ -8,39 +8,40 @@ import { Element } from "react-scroll";
 
 export const HomeFourSimpleSteps = () => {
   return (
-    <div className="fourSimpleSteps__container">
-      <Element name="oCoChodzi" />
-      <h2>Wystarczą 4 proste kroki</h2>
+    <Element name="oCoChodzi">
+      <div className="fourSimpleSteps__container">
+        <h2>Wystarczą 4 proste kroki</h2>
 
-      <div className="fourColumnsBackground">
-        <div className="fourColumns__wrapper">
-          <ColumnStep
-            title="Wybierz rzeczy"
-            description="ubrania, zabawki, sprzęt i inne"
-            alt="shirt"
-            src={shirt}
-          />
-          <ColumnStep
-            title="Spakuj je"
-            description="skorzystaj z worków na śmieci"
-            alt="bag"
-            src={bag}
-          />
-          <ColumnStep
-            title="Zdecyduj komu chcesz pomóc"
-            description="wybierz zaufane miejsce"
-            alt="search"
-            src={search}
-          />
-          <ColumnStep
-            title="Zamów kuriera"
-            description="kurier przyjedzie w dogodnym terminie"
-            alt="recycle"
-            src={recycle}
-          />
+        <div className="fourColumnsBackground">
+          <div className="fourColumns__wrapper">
+            <ColumnStep
+              title="Wybierz rzeczy"
+              description="ubrania, zabawki, sprzęt i inne"
+              alt="shirt"
+              src={shirt}
+            />
+            <ColumnStep
+              title="Spakuj je"
+              description="skorzystaj z worków na śmieci"
+              alt="bag"
+              src={bag}
+            />
+            <ColumnStep
+              title="Zdecyduj komu chcesz pomóc"
+              description="wybierz zaufane miejsce"
+              alt="search"
+              src={search}
+            />
+            <ColumnStep
+              title="Zamów kuriera"
+              description="kurier przyjedzie w dogodnym terminie"
+              alt="recycle"
+              src={recycle}
+            />
+          </div>
         </div>
+        <Button type="router" link="logowanie" btnText="ODDAJ RZECZY" />
       </div>
-      <Button type="router" link="logowanie" btnText="ODDAJ RZECZY" />
-    </div>
+    </Element>
   );
 };
